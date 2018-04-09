c9f3d05ba3c26193c112ee629c9ab7f0d3a54e8a
William Riancho <william@reptilians.io>
2018-04-09 22:41:32 +0200

    wip

--- start-sql-proxy.sh
+++ start-sql-proxy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./cloud_sql_proxy -instances=jeffrey-197808:europe-west1:api-production=tcp:3399 -credential_file=./sql-client.json
+cloud_sql_proxy -instances=jeffrey-197808:europe-west1:api-production=tcp:3399 -credential_file=./sql-client.json
